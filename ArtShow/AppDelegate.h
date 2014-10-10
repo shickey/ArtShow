396456e4e0799a98c974a957a228de3e0ec2a5b2
Sean Hickey <shickey.dev@gmail.com>
2014-10-09 21:47:08 -0500

    Initial Commit

---
+++ ArtShow/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  ArtShow
+//
+//  Created by Sean Hickey on 10/9/14.
+//  Copyright (c) 2014 Open Set Software. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+
+@end
+
---
+++ ArtShow/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  ArtShow
+//
+//  Created by Sean Hickey on 10/9/14.
+//  Copyright (c) 2014 Open Set Software. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
